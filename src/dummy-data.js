fab4b3d6aab00565456819d22354162c7d015c84
Justin Russell <Justin@Justins-MacBook.local>
2020-02-18 14:17:31 -0800

    partial stretch

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -1,5 +1,30 @@
 const dummyData = [
     {
+        username: "chrisdelia",
+        thumbnailUrl: 'https://scontent-sea1-1.cdninstagram.com/v/t51.2885-19/s320x320/76807418_2482786762010307_2886475712359825408_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com&_nc_ohc=MyagMOG8cNEAX-5wW-j&oh=4c50feb58524e9c6a068b8a3bc03685d&oe=5EE280D0',
+        imageUrl: 'https://scontent-sea1-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/81706186_633355804103864_7882572265414243961_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com&_nc_cat=108&_nc_ohc=JHpOqIGf4XUAX-S5vJq&oh=ad4700438be5ec4e69e1b6d33d1cb541&oe=5EE034E9',
+        likes: 134899,
+        timestamp: "January 21st 2020, 03:12:09 pm",
+        comments: [
+            {
+                username: "chrisdelia",
+                text: "No Dents"
+            },
+            {
+                username: "kyle_lowry7",
+                text: "I want my chips with the dip"
+            },
+            {
+                username: "aloc818",
+                text: "Are they filming Mad Max on there?"
+            },
+            {
+                username: "bryancallen",
+                text: "I box"
+            }
+        ]
+    },
+    {
         username: "philzcoffee",
         thumbnailUrl: 'https://scontent-lax3-2.cdninstagram.com/v/t51.2885-19/s150x150/67248626_346350186304601_6715901285419188224_n.jpg?_nc_ht=scontent-lax3-2.cdninstagram.com&_nc_ohc=tPD-mCU0-e4AX-iqOFz&oh=3535ba4a867c0afc9c67d0e504760359&oe=5EF8E9C8',
         imageUrl: 'https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80',
